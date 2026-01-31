7dee1c83088e52b97468347e8e8100d01902811a
AnindyaGB <anindya.gopal.b@gmail.com>
2026-01-30 20:48:51 -0500

    Add up to date resume pdf edit header text

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -13,10 +13,10 @@ const Header = () => {
             Software Engineer based in Toronto Canada
         </h1>
         <p>
-            Front-End / Full-Stack Software Engineer with 4+ years of professional experience building React and Next.js applications. Strong in JavaScript, UI design, accessibility, and API integration.         </p>
+            Front-End / Full-Stack Software Engineer with 5 years of professional experience building React and Next.js applications. Strong in JavaScript, UI design, accessibility, and API integration.         </p>
     <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>contact me<Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
-        <a href='/sample-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume<Image src={assets.download_icon} alt='' className='w-4' /></a>
+        <a href='/Anindya Bhattacharyya Resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume<Image src={assets.download_icon} alt='' className='w-4' /></a>
 
     
     </div>
